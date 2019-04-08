0f072db52d928b9e423d3eff29e7a7c649600556
David Phillips <zantier.tasa@gmail.com>
2019-04-08 02:01:32 +0100

    Add canvas, with a test animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	componentDidMount = () => {
+		this.ctx.canvas.width = 300;
+		this.ctx.canvas.height = 400;
+		this.lastTime = Date.now();
+		this.test = 0;
+
+		this.draw();
+	}
+
+	render = () => {
+		return (
+			<div className="App">
+				<canvas ref={canvas => this.ctx = canvas.getContext('2d', {alpha: false})}>
+					Your browser sucks.
+				</canvas>
+			</div>
+		);
+	}
+
+	draw = () => {
+		this.update();
+		this.ctx.fillStyle = '#000000';
+		this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+		this.ctx.fillStyle = '#cccccc';
+		var testWidth = 100;
+		this.ctx.fillRect(testWidth + testWidth * Math.cos(this.test), testWidth + testWidth * Math.sin(this.test), 10, 10);
+
+		requestAnimationFrame(this.draw);
+	}
+
+	update = () => {
+		var nowTime = Date.now();
+		var timeDiff = nowTime - this.lastTime;
+		this.lastTime = nowTime;
+
+		this.test += timeDiff/1000;
+	}
 }
 
 export default App;
